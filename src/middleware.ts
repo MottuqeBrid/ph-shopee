f60a7c5416b750f2656f646b088fbd37dae51305
Mottuqe Brid <mottuqebrid@gmail.com>
2025-08-21 19:21:57 +0600

    feat: Update session token retrieval for enhanced security

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("next-auth.session-token")?.value;
+  const token = request.cookies.get("__Secure-next-auth.session-token")?.value;
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
